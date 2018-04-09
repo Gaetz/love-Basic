443e81a529ca405bc1b1a01ed5d2750182315ac8
Gaetz <gaetz@rufflerim.com>
2018-04-09 19:14:38 +0200

    Simpler main

--- main.lua
+++ main.lua
@@ -1,14 +1,8 @@
 local img
-local width, height
 
 function love.load()
     img = love.graphics.newImage("assets/images/personnage.png")
   
-    width = love.graphics.getWidth()
-    height = love.graphics.getHeight()
-  
-    print("window width:", width)
-    print("window height:", height)
 end
 
 function love.update(dt)
